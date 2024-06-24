c92bf33ee36c0529e80995cbe1f7fbc7192644ea
Christelle18 <vigninouavodaho@gmail.com>
2024-06-24 14:19:00 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var v1 = 0;
+ function plus(){
+    v1+=1
+     document.getElementById('span1').textContent=v1
+ }
+
+function moins(){
+    v1=v1-1;
+    document.getElementById('span1').textContent=v1
+}
+
